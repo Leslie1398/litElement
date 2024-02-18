c4b5797f488c699435eaa10d1f2eb0dc85e3c81c
Leslie1398 <leslie.jack.anais@gmail.com>
2024-02-18 16:05:31 -0600

    add

--- rick and morty/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.use(express.static(__dirname)); // Sirve archivos estáticos desde tu directorio actual
-
-app.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
-});
